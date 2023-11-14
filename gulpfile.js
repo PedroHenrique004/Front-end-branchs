f2fad3e57736cf417c73d69f9497503a15aac0b9
Pedro Santos <124071877+PedroHenrique004@users.noreply.github.com>
2023-11-14 01:16:28 +0000

    adicionando a compilarSass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require ('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin'); 
 const uglify = require ('gulp-uglify');
 
-function comprimindoImagens(){
-    return gulp.src('./source/images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./build/images'))
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build./styles'));
 }
 
 function comprimiJS(){
@@ -16,5 +23,8 @@ function comprimiJS(){
 
 
 
-exports.images = comprimindoImagens;
-exports.javascript = comprimiJS;
+exports.default= function (){
+    gulp.watch('./source/styles/*.scss',{ignoreInitial: false}, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js',{ignoreInitial: false}, gulp.series(comprimiJS));
+    gulp.watch('./source/images/*' ,{ignoreInitial: false}, gulp.series(comprimindoImagens));
+}
